aaaabfc630528615c361a06fbce05c8dd527624a
Rasmus Moberg Naver <rasm653p@edu.campusvejle.dk>
2017-11-01 12:22:10 +0100

    Tilføjet grundlæggende design i html og css

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="css/styles.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CMS Test</title>
 </head>
 <body>
-    
+    <div class="container">
+    <header><img src="img/p3_protag.jpg" alt="Banner, en side om mine yndlings spil"></header>
+    <nav>
+        <ul>
+            <a href="#"><li>Home</li></a>
+            <a href="#"><li>Log in</li></a>
+            <a href="#"><li>About</li></a>
+            <a href="#"><li>Contact</li></a>
+        </ul>
+    </nav>
+    <main>
+        <article>
+            <img src="img/disgaea.jpg" alt="Disgaea">
+            <h2>Disgaea</h2>
+            <h3>01-11-2017 11:16</h3>
+            <p>Cake cupcake soufflé cookie sesame snaps candy canes ice cream jelly beans I love. Gummies sugar plum chocolate cake ice cream sweet roll liquorice croissant apple pie cheesecake. Topping brownie gummi bears tootsie roll. I love pudding croissant candy cupcake chocolate cake liquorice lollipop. Chocolate bar jelly cotton candy cupcake cake I love cake jelly. Powder chupa chups cheesecake. Croissant icing brownie bear claw caramels I love chocolate cake. Dragée danish jujubes I love donut chocolate cake muffin muffin. Jelly-o jujubes gummi bears carrot cake cookie chocolate marshmallow fruitcake. I love I love I love powder. Donut danish gingerbread bonbon I love. Sweet roll icing I love cupcake cake danish soufflé pudding. Chocolate cake sesame snaps chupa chups wafer marshmallow bonbon I love.</p>
+        </article>
+        <article>
+            <img src="img/mh4u_gore.jpg" alt="Monster Hunter 4 Ultimate">
+            <h2>Monster Hunter 4 Ultimate</h2>
+            <h3>01-11-2017 10:23</h3>
+            <p>Marzipan cotton candy toffee gummi bears. Tootsie roll jelly topping carrot cake gummies powder gummi bears chocolate cake. Halvah pastry pudding gummies. Pastry I love marzipan lemon drops bonbon. Carrot cake candy canes pastry biscuit caramels cake. I love lollipop cake I love. Liquorice chupa chups ice cream macaroon pie pastry tootsie roll sugar plum. Pudding gummies apple pie lemon drops. Candy canes cotton candy I love. Sugar plum sweet ice cream oat cake caramels jelly-o ice cream. Bonbon liquorice sweet pastry powder sweet roll dragée muffin icing. Cupcake cotton candy jelly-o toffee donut I love macaroon. Apple pie toffee marshmallow apple pie gingerbread sweet roll macaroon pie. Sesame snaps brownie lemon drops.</p>
+        </article>
+    </main>
+    <aside>
+        <div class="login">
+            <p>Username:</p>
+            <input type="text" name="username">
+            <p>Password:</p>
+            <input type="text" name="password">
+            <br>
+            <button type="button">Submit</button>
+        </div>
+    </aside>
+    <footer>
+        <h6>2017 copyright<h4>
+    </footer>
+    </div>
 </body>
 </html>
