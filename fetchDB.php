713b5dbb623f78be6c4b63b48cfc71fe5e5b60d2
Rasmus Moberg Naver <rasm653p@edu.campusvejle.dk>
2017-11-06 14:43:42 +0100

    Tilføjet funktionalitet til at vise time som dato og tidspunkt

--- fetchDB.php
+++ fetchDB.php
@@ -12,9 +12,9 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC)){ ?>
     <img src="img/<?php echo $row['imgSrc']; ?>.jpg" alt="<?php echo $row['imgAlt']; ?>">
     <h2><?php echo $row['heading']; ?></h2>
     <?php if(isset($row['username']) && !empty($row['username'])) { ?>
-        <h3><?php echo $row['time'], " by ", $row['username']; ?></h3> <?php 
+        <h3><?php echo date("G:i d-m-y", $row['time']), " by ", $row['username']; ?></h3> <?php 
         } else { ?>
-        <h3><?php echo $row['time'] ?> </h3> <?php
+        <h3><?php echo date("G:i d-m-y", $row['time']); ?> </h3> <?php
     } ?>
     <p><?php echo $row['articleText']; ?></p>
 </article>
