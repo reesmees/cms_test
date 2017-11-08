67732d7533c825664b36c3aa18ef4903e4afa830
Rasmus Moberg Naver <rasm653p@edu.campusvejle.dk>
2017-11-08 10:23:18 +0100

    Misc ændringer

--- register.php
+++ register.php
@@ -6,44 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register as a new user</title>
+    <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
 </head>
 <body>
-    <header><img src="img/p3_protag.jpg" alt="En side om mine yndlings spil"></header>
-    <nav>
-        <ul>
-            <a href="index.php"><li>Home</li></a>
-            <a href="#"><li>Log in</li></a>
-            <a href="#"><li>About</li></a>
-            <a href="#"><li>Contact</li></a>
-        </ul>
-    </nav>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="register">
-        <ul>
-            <li><label for="username">Enter username: <span class="required">*</span></label>
-                <input type="text" name="username" placeholder="Enter username here" />
-            </li>
-            <li><label for="password">Enter password: <span class="required">*</span></label>
-                <input type="password" name="password" placeholder="Enter password here" />
-            </li>
-            <li><label for="password2">Confirm password: <span class="required">*</span></label>
-                <input type="password" name="password2" placeholder="Confirm password here" />
-            </li>
-            <li>
-                <br>
-                <input type="submit" value="Register" />
-            </li>
-        </ul>
-    </form>
+    <div class="container">
+        <header><img src="img/p3_protag.jpg" alt="En side om mine yndlings spil"></header>
+        <nav>
+            <ul>
+                <a href="index.php"><li>Home</li></a>
+                <a href="#"><li>Log in</li></a>
+                <a href="#"><li>About</li></a>
+                <a href="#"><li>Contact</li></a>
+            </ul>
+        </nav>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="register">
+            <ul>
+                <li><label for="username">Enter username: <span class="required">*</span></label>
+                    <input type="text" name="username" placeholder="Enter username here" />
+                </li>
+                <li><label for="password">Enter password: <span class="required">*</span></label>
+                    <input type="password" name="password" placeholder="Enter password here" />
+                </li>
+                <li><label for="password2">Confirm password: <span class="required">*</span></label>
+                    <input type="password" name="password2" placeholder="Confirm password here" />
+                </li>
+                <li>
+                    <br>
+                    <input type="submit" value="Register" />
+                </li>
+            </ul>
+        </form>
+    </div>
     <?php
     //Tjek om der er blevet indtastet noget overhovedet
-    if(isset($_POST['username'])) {
+    if(!empty($_POST['username'])) {
         //Set indtastet information til variable
         $formUsername = $_POST['username'];
         $formPassword = $_POST['password'];
         $formPassword2 = $_POST['password2'];
         //Check om de to indtastede passwords er de samme
         if($formPassword != $formPassword2) {
-            echo "Your entered passwords didn't match.";
+            echo "<div class=\"error\">Your entered passwords didn't match.</div>";
         } else {
             require_once "connect.php";
             //Undersøg om der er eksisterende brugere med det indtastede brugernavn
@@ -56,11 +59,11 @@
                 $statement->bindParam(1, $formUsername);
                 $statement->bindParam(2, $formPassword);
                 $statement->execute();
-                echo "Your user has been registered.";
+                echo "<div class=\"error\">Your user has been registered.</div>";
                 header("Refresh:3; url=index.php");
             } else {
                 //Hvis brugernavnet allerede er taget
-                echo "A user with that username already exists.";
+                echo "<div class=\"error\">A user with that username already exists.</div>";
             }
         }
     }?>
